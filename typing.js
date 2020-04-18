b800159a3554f3e61528e0586ad5c27106d1cdea
Julian Mateo Ballesteros Rojas <mate_balles@hotmail.com>
2020-04-18 00:54:28 -0500

    html error

--- typing.js
+++ typing.js
@@ -9,18 +9,18 @@ function autoType(elementClass, typingSpeed){
     var text = thhis.text().trim().split('');
     var amntOfChars = text.length;
     var newString = "";
-    thhis.text("|");
+    thhis.text("|")
     setTimeout(function(){
       thhis.css("opacity",1);
       thhis.prev().removeAttr("style");
       thhis.text("");
-      for(var i = 0; i < amntOfChars; i++){
+      for(var i = 0; i < amntOfChars; i+){
         (function(i,char){
           setTimeout(function() {        
             newString += char;
             thhis.text(newString);
           },i*typingSpeed);
-        })(i+1,text[i]);
+        })(i+100,text[i]);
       }
     },1500);
   }
